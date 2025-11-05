f54b424d6bf833e17bf3d4947f9e566f041961cf
AlefOm <agomespro1@gmail.com>
2025-11-04 21:10:03 -0300

    Criados atributos 'nome' e 'idade'

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,23 @@
 class Pessoa:
+    
+    # Cria os atributos de dados (features, campos):
+    def __init__(self, nome = None, idade = 44):
+
+        # Podemos garantir a existencia do campo sem ter valor para ele atribuindo
+        # o valor None:
+        self.nome = None
+    
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+
+    # No momento da construcao da classe, a funcao __init__ eh passada. Logo,
+    # eh possivel atribuir valores aos atributos de dados nesse momento.
+    p = Pessoa('Carlos Augusto')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+
+    # Eh possivel acessar os atributos criados atraves do proprio objeto:
+    print(p.nome)
